329b26e7efdc81c94e669e9da8307eccebb8f471
Isfandiyor777 <139679142+Isfandiyor777@users.noreply.github.com>
2023-08-05 19:48:34 +0300

    Homework

---
+++ Kiber One.py
@@ -0,0 +1,13 @@
+num = 787
+num2 = 143
+
+primer = (num * num2)
+
+if primer == 800:
+    print("Correct")
+elif primer == 774:
+    print("GG")
+elif primer == 10231:
+    print("Good")
+else:
+    print("BAAAD")
